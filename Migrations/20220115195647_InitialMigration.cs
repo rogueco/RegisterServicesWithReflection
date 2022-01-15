a71c1ddb2a7ac031b1d527cd8f8b08a04df47092
Tom Fletcher <tom@tomfletcher.tech>
2022-01-15 20:33:16 +0000

    Containerised solution & added DB support

---
+++ Migrations/20220115195647_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegisterServicesWithReflection.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-RegisterServicesWithReflection
+#Inject services using reflection:
+#### Purpose of project
+- Show different ways on how to register services ready for DI
+- Using reflection to register services
+- Four different ways of registering services
+- Each method is in a separate branch
+
+If you want to test the functionality of the API you can run the following command and it will spin the Database up in a docker container.
+```bash
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
+```
+
+API: http://localhost:8002/swagger/index.html
+
+Otherwise, you can just build the solution
