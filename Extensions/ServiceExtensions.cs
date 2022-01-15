cc4792ae387fda19589b3739a469be0021f0da43
Tom Fletcher <tom@tomfletcher.tech>
2022-01-15 19:44:06 +0000

    Static Extension

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+// <copyright file="ServiceExtensions.cs" company="Commerce Control Limited">
+// Copyright (c) 2022 All Rights Reserved
+// </copyright>
+// <author>Tom Fletcher, Software Engineer</author>
+
+using RegisterServicesWithReflection.Services.Implementations;
+using RegisterServicesWithReflection.Services.Interfaces;
+
+namespace RegisterServicesWithReflection.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<ICustomerService, CustomerService>();
+        services.AddScoped<IInventoryService, InventoryService>();
+        services.AddSingleton<IPaymentService, PaymentService>();
+        services.AddTransient<IOrderService, OrderService>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RegisterServicesWithReflection.Data;
+using RegisterServicesWithReflection.Extensions;
 using RegisterServicesWithReflection.Services.Implementations;
 using RegisterServicesWithReflection.Services.Interfaces;
 
@@ -18,10 +19,7 @@ builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connecti
 
 
 // Add Services
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddScoped<IInventoryService, InventoryService>();
-builder.Services.AddSingleton<IPaymentService, PaymentService>();
-builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
